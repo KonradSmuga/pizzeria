accd76db3933bbe7069859ec1a759549f28c565d
unknown <smuggk@gmail.com>
2020-12-17 13:10:10 +0100

    cart, changes of html/css. Adding few selects

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,4 +1,4 @@
-/* global Handlebars */
+/* global Handlebars, dataSource */
 
 const utils = {}; // eslint-disable-line no-unused-vars
 
@@ -14,7 +14,7 @@ utils.createPropIfUndefined = function(obj, key, value = []){
   }
 };
 
-utils.serializeFormToObject = function(form){
+uutils.serializeFormToObject = function(form){
   let output = {};
   if (typeof form == 'object' && form.nodeName == 'FORM') {
     for (let field of form.elements) {
@@ -29,13 +29,26 @@ utils.serializeFormToObject = function(form){
         } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
           utils.createPropIfUndefined(output, field.name);
           output[field.name].push(field.value);
-        }
+        } else if(!output[field.name]) output[field.name] = [];
       }
     }
   }
   return output;
 };
 
+utils.convertDataSourceToDbJson = function(){
+  const productJson = [];
+  for(let key in dataSource.products){
+    productJson.push(Object.assign({id: key}, dataSource.products[key]));
+  }
+
+  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+};
+
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+Handlebars.registerHelper('joinValues', function(input, options) {
+  return Object.values(input).join(options.fn(this));
 });
